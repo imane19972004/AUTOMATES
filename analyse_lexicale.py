82abac900f33094137e4c6f7f5ecd35d384b5957
user <amanarodia@gmail.com>
2025-05-27 14:33:05 +0200

    ajout de tests pour lenalyse syntaxique

--- analyse_lexicale.py
+++ analyse_lexicale.py
@@ -1,53 +1,62 @@
 import sys
 from sly import Lexer
 
-
 class FloLexer(Lexer):
-	# Noms des lexèmes (sauf les litéraux). En majuscule. Ordre non important
-	tokens = { IDENTIFIANT, ENTIER, ECRIRE,INFERIEUR_OU_EGAL }
+    tokens = {
+        IDENTIFIANT, ENTIER, ECRIRE, LIRE, RETOURNER, TANTQUE, FONCTION,
+        INFERIEUR_OU_EGAL, SUPERIEUR_OU_EGAL, EGAL_EGAL, DIFFERENT,
+        ET, OU, NON, SI, SINON, VRAI, FAUX, ENTIER_TYPE, BOOLEEN_TYPE,
+        CHAINE
+    }
 
-	#Les caractères litéraux sont des caractères uniques qui sont retournés tel quel quand rencontré par l'analyse lexicale. 
-	#Les litéraux sont vérifiés en dernier, après toutes les autres règles définies par des expressions régulières.
-	#Donc, si une règle commence par un de ces littérals (comme INFERIEUR_OU_EGAL), cette règle aura la priorité.
-	literals = { '+','*','(',')',";" }
-	
-	# chaines contenant les caractère à ignorer. Ici espace et tabulation
-	ignore = ' \t'
+    literals = {'+', '-', '*', '/', '%', '(', ')', ';', '=', '{', '}', ',', '>', '<'}
+    ignore = ' \t'
+    ignore_comment = r'\#.*'
 
-	# Expressions régulières correspondant au différents Lexèmes par ordre de priorité
-	INFERIEUR_OU_EGAL= r'<='
-	
-	@_(r'0|[1-9][0-9]*')
-	def ENTIER(self, t):
-		t.value = int(t.value)
-		return t
+    INFERIEUR_OU_EGAL = r'<='
+    SUPERIEUR_OU_EGAL = r'>='
+    EGAL_EGAL = r'=='
+    DIFFERENT = r'!='
 
-    	# cas général
-	IDENTIFIANT = r'[a-zA-Z][a-zA-Z0-9_]*' #en général, variable ou nom de fonction
+    CHAINE = r'"[^\n"]*"'
 
-	# cas spéciaux:
-	IDENTIFIANT['ecrire'] = ECRIRE
-	
-	#Syntaxe des commentaires à ignorer
-	ignore_comment = r'\#.*'
+    @_(r'0|[1-9][0-9]*')
+    def ENTIER(self, t):
+        t.value = int(t.value)
+        return t
 
-	# Permet de conserver les numéros de ligne. Utile pour les messages d'erreurs
-	@_(r'\n+')
-	def ignore_newline(self, t):
-		self.lineno += t.value.count('\n')
+    IDENTIFIANT = r'[a-zA-Z][a-zA-Z0-9_]*'
 
-	# En cas d'erreur, indique où elle se trouve
-	def error(self, t):
-		print(f'Ligne{self.lineno}: caractère inattendu "{t.value[0]}"',file=sys.stderr)
-		self.index += 1
-		exit(1)
-	
+    IDENTIFIANT['ecrire'] = ECRIRE
+    IDENTIFIANT['lire'] = LIRE
+    IDENTIFIANT['si'] = SI
+    IDENTIFIANT['sinon'] = SINON
+    IDENTIFIANT['retourner'] = RETOURNER
+    IDENTIFIANT['tantque'] = TANTQUE
+    IDENTIFIANT['fonction'] = FONCTION    # <-- Ajout ici
+    IDENTIFIANT['et'] = ET
+    IDENTIFIANT['ou'] = OU
+    IDENTIFIANT['non'] = NON
+    IDENTIFIANT['Vrai'] = VRAI
+    IDENTIFIANT['Faux'] = FAUX
+    IDENTIFIANT['entier'] = ENTIER_TYPE
+    IDENTIFIANT['booleen'] = BOOLEEN_TYPE
+
+    @_(r'\n+')
+    def ignore_newline(self, t):
+        self.lineno += t.value.count('\n')
+
+    def error(self, t):
+        print(f'Ligne {self.lineno}: caractère inattendu "{t.value[0]}"', file=sys.stderr)
+        self.index += 1
+        exit(1)
+
 if __name__ == '__main__':
-	if len(sys.argv) < 2:
-		print("usage: python3 analyse_lexicale.py NOM_FICHIER_SOURCE.flo")
-	else:
-		with open(sys.argv[1],"r") as f:
-			data = f.read()
-			lexer = FloLexer()
-			for tok in lexer.tokenize(data):
-				print(tok)
+    if len(sys.argv) < 2:
+        print("usage: python3 analyse_lexicale.py NOM_FICHIER_SOURCE.flo")
+    else:
+        with open(sys.argv[1], 'r') as f:
+            data = f.read()
+            lexer = FloLexer()
+            for tok in lexer.tokenize(data):
+                print(tok)
